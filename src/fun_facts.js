d697923a09e40d90b159f7bccea06ac05558b539
Jada Quandt <jadaquandt@gmail.com>
2021-03-19 20:02:16 -0400

    adding more women, cleaning up files

--- src/fun_facts.js
+++ src/fun_facts.js
@@ -4,6 +4,8 @@ const funFacts = [
       "Ada Lovelace was the only child of poet Lord Byron and Lady Byron",
       "Ada, the programming language created by the U.S. Department of Defense is named in honor of Ada Lovelace",
       "In 1987, Anita Borg founded Systers, the first email network for women in technology.",
+      "Ellen Ochoa has six schools named after her, and she received even the Presidential Distinguished Rank Award from the federal government for senior executives",
+      "Without Hedy Lamarr, there would be no wireless communication in our modern world",
     ],
   },
 ];
